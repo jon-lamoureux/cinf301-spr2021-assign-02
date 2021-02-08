f2e9be4dda46c35ba5024961bf39bf04b48ab4a8
naponfh <69806919+naponfh@users.noreply.github.com>
2021-02-08 13:28:19 -0500

    Data saves between pages using javascript and cookies

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-// source: https://css-tricks.com/snippets/javascript/get-url-variables/
-function getQueryVariable(variable)
-{
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
+// Base code source: https://stackoverflow.com/questions/30419929/setting-cookies-with-form-input
+
+var today = new Date();
+var expiry = new Date(today.getTime() + 1 * 24 * 3600 * 1000); // current date + 1 day
+var expiryOld = new Date(today.getTime() - 1 * 24 * 3600 * 1000); // current date - 1 day
+
+function setCookie(name, value) {
+  document.cookie=name + "=; path=/; expires=" + expiryOld.toGMTString();
+  document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
+}
+function putCookie(form) {
+  setCookie("postTitle", form[0].posttitle.value);
+  setCookie("postUser", form[0].postuser.value);
+  setCookie("postContent", form[0].postcontent.value);
+  window.location.replace("post.html");
+  return true;
 }
